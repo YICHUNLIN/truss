19ca9ccb17d89ba020cd3f9df1f7157bf06b053d
Vic Lin <yichun03@gmail.com>
2017-03-15 11:59:27 +0800

    first commit

---
+++ 0551283_main.py
@@ -0,0 +1,170 @@
+#encoding = utf-8
+'''
+Author  Vic Lin
+
+at 2017/3/5
+
+'''
+import math
+import configparser
+
+# 一個座標
+class Point(object):
+    x = 0.0
+    y = 0.0
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+# 一個節點
+class Node(object):
+    # 結點名稱
+    name = ""
+    # 連接桿件
+    connect = []
+    def __init__(self, point , name):
+        self.point = point
+        self.name = name
+        #print("node %s (%.2lf,%.2lf)" % (self.name, self.point.x, self.point.y))
+
+    # 兩個結點的距離(長度)
+    def distance(self,anotherNode):
+        return math.sqrt((anotherNode.point.x - self.point.x)*(anotherNode.point.x - self.point.x) + (anotherNode.point.y - self.point.y)*(anotherNode.point.y - self.point.y))
+
+    # 加入一個桿件
+    def addmember(self, member):
+        self.connect.append(member)
+
+# 一個趕件
+class Member(object):
+    # 桿件名稱
+    name = ""
+    # 桿件長度
+    length = 0.0
+    # 桿件斷面
+    area = 0.0
+    # 彈性模數
+    elm = 0.0
+    # E*A/L
+    eal = 0.0
+    # 始 -> 末, 名字 ,內徑, 外徑
+    def __init__(self, startnode, endnode, name, iR, oR, elm = 2.04*1000000):
+
+        self.startNode = startnode
+        self.endNode = endnode
+        self.name = name
+        self.calLength()
+        self.area = 3.1415926 * abs((oR/2)*(oR/2) - (iR/2)*(iR/2))
+        self.elm = elm
+        self.eal = self.elm * self.area / self.length
+        #print("Member %s ,leng %.2lf ,area %.2lf" %(self.name, self.length, self.area))
+
+    def calLength(self):
+        self.length = self.startNode.distance(self.endNode)
+
+    def addSelftoNodes(self):
+        self.startNode.addmember(self)
+        self.endNode.addmember(self)
+
+
+# 一組桁架
+class Truss(object):
+    infile = "0551283IN.txt"
+    outfile = "0551283OUT.txt"
+
+    # 所有節點
+    nodes = []
+    # 所有桿件
+    members = []
+    def __init__(self, infile= "0551283IN.txt", outfile= "0551283OUT.txt"):
+        self.infile = infile
+        self.outfile = outfile
+
+    # 加入一個節點
+    def addnode(self, node):
+        self.nodes.append(node)
+
+    # 加入一個桿件
+    def addmember(self, member):
+        member.addSelftoNodes()
+        self.members.append(member)
+
+    # 印出所有元件
+    def components(self):
+        print("------Nodes------")
+        for n in self.nodes:
+            print("%s(%.5lf,%.5lf)" % (n.name, n.point.x, n.point.y))
+
+        print("-----Members------")
+        for m in self.members:
+            print("%s - (%s,l %.5lf, a %.5lf, E*A/L %.5lf) - %s " % (m.startNode.name ,m.name, m.length, m.area, m.eal, m.endNode.name))
+
+    # 從檔案取得桁架資訊
+    def getTrussFromFile(self):
+        try:
+            f = open(self.infile,"r")
+            while True:
+                line = f.readline()
+                if not line:
+                    break
+                arr = line.replace('\n',"").split(",")
+                if  'n' in arr[0]:
+                    self.addnode(Node(Point(float(arr[1]),float(arr[2])),arr[0]))
+                elif( 'm' in arr[0]):
+                    ns = self.getNodeByName(arr[1])
+                    ne = self.getNodeByName(arr[2])
+                    self.addmember(Member(ns,ne,arr[0],float(arr[3]),float(arr[4])))
+            f.close()
+        except:
+            print("File exception")
+
+    # 將趕件寫入檔案
+    def writeMemberToFile(self):
+        try:
+            f = open(self.outfile, "w")
+            for m in self.members:
+                f.write("%s,L %.5lf,A %.5lf,E*A/L %.5lf\n" % (m.name, m.length, m.area, m.eal))
+            f.close()
+        except:
+            print("File exception")
+
+    # 根據名字取得節點
+    def getNodeByName(self, name):
+        for n in self.nodes:
+            if n.name == name:
+                return n
+        return None
+
+    # 排序
+    def sortMember(self):
+        for i in range(0, len(self.members)):
+            for j in range(0, len(self.members)):
+                # 去除英文字
+                t1 = int(self.members[i].name.replace("m",""))
+                t2 = int(self.members[j].name.replace("m",""))
+                if  t1 < t2:
+                    tmp = self.members[j]
+                    self.members[j] = self.members[i]
+                    self.members[i] = tmp
+
+    def getTrussFromini(self):
+        config=configparser.ConfigParser()
+        
+                
+
+
+
+def main():
+    try:
+        t = Truss("0551283IN.txt", "0551283OUT.txt")
+        t.getTrussFromFile()
+        t.sortMember()
+        t.components()
+        t.writeMemberToFile()
+    except:
+        print("Error in main function")
+
+
+if __name__ == "__main__":
+    main()
